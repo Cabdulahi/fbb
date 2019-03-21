445b9afdf59cf1f63e616c67724ca8b8f4ee477c
Cabdulahi Sharif Mohammed <33965166+Cabdulahi@users.noreply.github.com>
2019-03-21 17:19:28 +0300

    Add files via upload

---
+++ fbb+.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# coding=utf-8
+#///.coded by cabdulahi sharif
+import time
+import os,sys
+from cadow import fbbrute
+
+
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk)) 
+
+green = '\033[32;1m'
+
+gta = '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+  #//Colors 
+white = '\033[1;37m' # White 
+red = '\033[31m' # red
+orange = '\033[33m' # orange
+blue = '\033[34m' # blue
+p  = '\033[35m' # purple
+C  = '\033[36m' # cyan
+
+idgroup = []
+
+
+
+class fbr:
+ def __init__(self):
+     
+     time.sleep(1)
+     print
+     try:
+         fbbrute.shit()
+     except Exception as h:
+         print h
+
+
+
+
+
+       
+       
+       
+
+       
+if __name__ == "__main__":
+	fbr()
